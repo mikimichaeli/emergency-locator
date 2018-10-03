84d82200d78f33f93eb2a7732fc8383784abbeb4
Bar Admoni <badmoni@cisco.com>
2018-10-03 16:43:06 +0300

    Finsihed electron app

--- operator/client/components/createPhoneCall.js
+++ operator/client/components/createPhoneCall.js
@@ -9,8 +9,13 @@ import MapHolder from './mapHolder';
 import Phone from 'material-ui/svg-icons/communication/phone';
 
 
+const openMapEvent = new CustomEvent("openMap");
+const closeMapEvent = new CustomEvent("closeMap");
+
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:3000/',
+    // baseURL: 'https://wll4mj4ekd.execute-api.eu-west-1.amazonaws.com/v1/',
+    credentials: false
 });
 
 const PULL_RATE = 300;
@@ -23,14 +28,17 @@ const STATUS = {
     CONNECTING: 'connecting',
     ERROR: 'error',
 }
+const buttonStyle = {
+    minWidth: 190
+}
 const LOCK_STATUS = [STATUS.SENDING, STATUS.WAITING, STATUS.OPENED, STATUS.CONNECTING];
 
 export default class CreatePhoneCall extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { phoneNumber: '', status: STATUS.INIT, disabledPhoneNumber: false, location: {} };
-        this.phoneCallID = null;
+        this.state = { phoneNumber: '+972', status: STATUS.INIT, disabledPhoneNumber: false, location: {} };
+        window.phoneCallID = null;
         window.checkInterval = null;
     }
 
@@ -74,6 +82,7 @@ export default class CreatePhoneCall extends React.Component {
         }
 
         this.setState({ status: STATUS.INIT });
+        window.dispatchEvent(closeMapEvent);
     }
 
     handleChange = (event) => {
@@ -81,10 +90,6 @@ export default class CreatePhoneCall extends React.Component {
             phoneNumber: event.target.value
         });
     };
-
-    handleClose = () => {
-
-    }
 
     componentWillUnmount() {
         if (window.checkInterval) {
@@ -97,44 +102,44 @@ export default class CreatePhoneCall extends React.Component {
         let button, map;
         switch (this.state.status) {
             case STATUS.INIT:
-                button = <RaisedButton label="Send Link" primary={true} onClick={this.sendSMS} />;
+                button = <RaisedButton label="Send Link" primary={true} onClick={this.sendSMS} buttonStyle={buttonStyle} />;
                 map = undefined;
                 break;
             case STATUS.SENDING:
-                button = <RaisedButton label="Cancel - Sending" secondary={true} icon={<CircularProgress size={25} onClick={this.cancelWhileRunning} />} />;
+                button = <RaisedButton label="Cancel - Sending" secondary={true} icon={<CircularProgress size={25} onClick={this.cancelWhileRunning} buttonStyle={buttonStyle} />} />;
                 map = undefined;
                 break;
             case STATUS.WAITING:
-                button = <RaisedButton label="Cancel - Waiting" secondary={true} icon={<CircularProgress size={25} />} onClick={this.cancelWhileRunning} />;
+                button = <RaisedButton label="Cancel - Waiting" secondary={true} icon={<CircularProgress size={25} />} onClick={this.cancelWhileRunning} buttonStyle={buttonStyle} />;
                 map = undefined;
                 break;
             case STATUS.OPENED:
-                button = <RaisedButton label="Close" primary={true} onClick={this.cancelWhileRunning} />;
+                button = <RaisedButton label="Close" primary={true} onClick={this.cancelWhileRunning} buttonStyle={buttonStyle} />;
                 map = <MapHolder location={this.state.location} />;
+                window.dispatchEvent(openMapEvent);
                 break;
             case STATUS.REJECTED:
-                button = <RaisedButton label="Rejected" secondary={true} onClick={this.sendSMS} />;
+                button = <RaisedButton label="Rejected" secondary={true} onClick={this.sendSMS} buttonStyle={buttonStyle} />;
                 map = undefined;
                 break;
             case STATUS.CONNECTING:
-                button = <RaisedButton label="Connecting" backgroundColor={"#AED581"} />;
+                button = <RaisedButton label="Connecting" backgroundColor={"#AED581"} buttonStyle={buttonStyle} />;
                 map = undefined;
                 break;
             case STATUS.ERROR:
-                button = <RaisedButton label="Error" secondary={true} onClick={this.sendSMS} />;
+                button = <RaisedButton label="Error" secondary={true} onClick={this.sendSMS} buttonStyle={buttonStyle} />;
                 map = undefined;
                 break;
             default:
-                button = <RaisedButton label="Error" secondary={true} onClick={this.sendSMS} />;
+                button = <RaisedButton label="Error" secondary={true} onClick={this.sendSMS} buttonStyle={buttonStyle} />;
                 map = undefined;
                 break;
         }
 
         return <div>
             <Paper zDepth={1}>
-                <h3><Phone /> Emergency Locator</h3>
                 <TextField
-                    style={{ marginRight: 10, marginLeft: 10 }}
+                    style={{ marginRight: 10, marginLeft: 10, width: 280 }}
                     value={this.state.phoneNumber}
                     onChange={this.handleChange}
                     disabled={LOCK_STATUS.includes(this.state.status)}
--- operator/client/components/notFoundPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class NotFoundPage extends React.Component {
-    render() {
-        return <h1>404 page!</h1>
-    }
-}
